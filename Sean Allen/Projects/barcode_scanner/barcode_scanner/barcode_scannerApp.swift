fba95abf127f75455a4cf5802d847ece1dd8e64c
Tino Krzelj <tino.krzelj@icloud.com>
2023-10-02 16:50:36 +0400

    Add barcode_scanner initial project

---
+++ Sean Allen/Projects/barcode_scanner/barcode_scanner/barcode_scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  barcode_scannerApp.swift
+//  barcode_scanner
+//
+//  Created by Tino Krzelj on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct barcode_scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
